d53414cfbc7ad1e694a7058ff5d25f48700818a6
KeyurMistry <keyurmist@gmail.com>
2022-05-29 18:02:45 +0100

    function to call local storage for recent city

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  let NowMoment = moment().format("1"); //this pulls the current date for the recent info
+
+  //this add 5 days to the moment to show the forecast for 5 days
+  let day1 = moment().add(1, "days").format("1");
+  let day2 = moment().add(2, "days").format("1");
+  let day3 = moment().add(3, "days").format("1");
+  let day4 = moment().add(4, "days").format("1");
+  let day5 = moment().add(5, "days").format("1");
+
+  let city;
+  let cities;
+
+  //this function loads the most recently searched city in the local storage
+  function loadMostRecent() {
+    let lastSearch = localStorage.getItem("mostRecent");
+    if (lastSearch) {
+      city = lastSearch;
+      lastSearch();
+    } else {
+      city = "London";
+      search();
+    }
+  }
+});
